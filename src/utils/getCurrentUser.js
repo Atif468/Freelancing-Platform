2519c738a14132f7af28ad7c1d63d71edd4865e7
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-09-17 10:12:48 +0530

    Delete src directory

--- src/utils/getCurrentUser.js
+++
@@ -1,4 +0,0 @@
-const getCurrentUser=()=>{
-return JSON.parse(localStorage.getItem("currentUser"));
-}
-export default getCurrentUser;
--- src/utils/newRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-//backend port number
-const newRequest=axios.create({
-    baseURL:"http://localhost:8080/api/",
-    withCredentials:true,
-})
-
-export default newRequest;
